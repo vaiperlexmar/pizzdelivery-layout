826b7c71abccdb26901f589b88b4c0c522831ff3
vaiperlexmar <vaiperlexmar@gmail.com>
2023-12-27 23:37:46 +0300

    style: Add animation of header for opening a menu

--- src/components/hamburger.mjs
+++ src/components/hamburger.mjs
@@ -2,19 +2,20 @@
 
 const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".header__menu");
-const topNavHeader = document.querySelector(".header__topnav");
+const circle = document.querySelector(".header__clip-path"); // clip-path animation of header
 
 function openMenu() {
   if (menu.classList.contains("header__menu_disabled")) {
     hamburger.classList.add("hamburger_open");
     menu.classList.remove("header__menu_disabled");
     menu.classList.add("header__menu_enabled");
-    topNavHeader.style.backgroundColor = "#f7d22d";
+    circle.style.clipPath = "circle(200% at 0 0)";
   } else {
     hamburger.classList.remove("hamburger_open");
     menu.classList.remove("header__menu_enabled");
     menu.classList.add("header__menu_disabled");
-    topNavHeader.style.backgroundColor = "#fff";
+
+    circle.style.clipPath = "circle(0% at 0 0)";
   }
 }
 
