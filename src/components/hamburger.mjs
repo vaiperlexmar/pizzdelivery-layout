61d3de0f1d5e7e69a4637d9852fcb9aec6e31e2b
vaiperlexmar <vaiperlexmar@gmail.com>
2023-12-27 23:02:56 +0300

    style: Add animation of hamburger.
    Make it cross when it open a menu. It's pixel perfect, but need to additional styling

--- src/components/hamburger.mjs
+++ src/components/hamburger.mjs
@@ -6,10 +6,12 @@ const topNavHeader = document.querySelector(".header__topnav");
 
 function openMenu() {
   if (menu.classList.contains("header__menu_disabled")) {
+    hamburger.classList.add("hamburger_open");
     menu.classList.remove("header__menu_disabled");
     menu.classList.add("header__menu_enabled");
     topNavHeader.style.backgroundColor = "#f7d22d";
   } else {
+    hamburger.classList.remove("hamburger_open");
     menu.classList.remove("header__menu_enabled");
     menu.classList.add("header__menu_disabled");
     topNavHeader.style.backgroundColor = "#fff";
